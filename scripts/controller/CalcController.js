017b0dfb4196811b29a2f6f8f6a3f4bd4cbdaba2
pedro-ds <pedro.hnq.dias@gmail.com>
2018-07-03 14:28:40 -0300

    Refatorando a função dos números

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -22,6 +22,7 @@ class CalcController{
         }, 1000);
     }
 
+    //Criando método "para cada" evento do mouse
     addEventListenerAll(element, events, fn){
         
         events.split(' ').forEach(event =>{
@@ -30,19 +31,58 @@ class CalcController{
         });
 
     }
+    //Limpa qualquer dado no display da calculadora.
     clearAll(){
         this._operation = [];
     }
+    //Deleta a última entrada.
     clearEntry(){
         this._operation.pop();
     }
-    addOperation(value){
-        this._operation.push(value);
+    //Pega a última posição do array:
+    getLastOperation(){
+        return this._operation[this._operation.length - 1];
     }
+    setLastOperation(value){
+
+        this._operation[this._operation.length - 1] = value;
+    }
+
+    //Método que será utilizado para verificar se o último operador É DIFERENTE de número
+    isOperator(value){
+        return (['+', '-', '*', '/', '%'].indexOf(value) > -1); 
+    } 
+
+    //Cria um array "para operações".
+    addOperation(value){
+
+        console.log('A', isNaN(this.getLastOperation()));
+        
+        if(isNaN(this.getLastOperation())){
+            //Para quando for string. 
+            if(this.isOperator(value)){
+                
+                this._setLastOperation(value);
+
+            }else if(isNaN(value)) {
+                
+                console.log(value);
+            }else{
+                this._operation.push(value);
+            }
+
+        } else {
+            //Para quando for número. 
+            let newValue = this.getLastOperation().toString() + value.toString();
+            this.setLastOperation(parseInt(newValue));
+        }
+        console.log(this._operation);
+    }
+    //Default do Switch/Case.
     setError(){
         this.displayCalc = "Error";
     }
-
+    //Switch case para botões/operações
     execBtn(value){
 
         switch(value){
@@ -55,27 +95,31 @@ class CalcController{
             break;
 
             case 'soma':
-                this.soma();
+                this.addOperation('+');
             break;
 
             case 'subtracao':
-                this.subtracao();
+                this.addOperation('-');
             break;
 
             case 'multiplicacao':
-                this.multiplicacao();
+                this.addOperation('*');
             break;
 
             case 'divisao':
-                this.divisao();
+                this.addOperation('/');
             break;
 
             case 'porcento':
-                this.porcento();
+                this.addOperation('%');
             break;
 
             case 'igual':
                 this.igual();
+            break;
+
+            case 'ponto':
+                this.addOperation('.');
             break;
 
             case '0':
@@ -98,6 +142,7 @@ class CalcController{
         }
     }
 
+    //Eventos do Mouse
     initButtonEvents(){
 
         let buttons = document.querySelectorAll("#buttons > g, #parts > g");
@@ -117,6 +162,7 @@ class CalcController{
         });
     }
 
+    //Data e Hora na Calculadora
     setDisplayDateTime(){
         this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
             day: "2-digit",
@@ -127,6 +173,7 @@ class CalcController{
         this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
     }
 
+    //Getters e Setters
     get displayTime(){
         return this._timeEl.innerHTML;
     }
